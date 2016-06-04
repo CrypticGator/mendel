a227caa0c05010cba36cd1b4769b99052567131c
Irae <irae@irae.pro.br>
2016-06-03 19:04:39 -0700

    Add script to run mendel with extractify; add --no-config option

---
+++ examples/full-example/run-browserify.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+browserify      "isomorphic/base/main.js"                        \
+    --external  "react"                                          \
+    --external  "react-dom"                                      \
+    --outfile   "build/client/base/main.js"                      \
+    --transform "babelify"                                       \
+                                                                 \
+    --plugin [  "mendel-browserify"                              \
+                --no-config                                      \
+                --basetree      "isomorphic/base"                \
+                --variations [                                   \
+                    --bucket_A                                   \
+                ]                                                \
+                --variationsdir "isomorphic/variations"          \
+                --outdir        "build"                          \
+                --bundlesoutdir "client"                         \
+    ]                                                            \
+                                                                 \
+    --plugin [  "extractify"                                     \
+                --lazy [                                         \
+                    [                                            \
+                        --outfile "build/client/base/lazy.js"    \
+                        --entries [                              \
+                            "isomorphic/base/components/lazy.js" \
+                        ]                                        \
+                    ]                                            \
+                ]                                                \
+    ]                                                            \
+                                                                 \
+
